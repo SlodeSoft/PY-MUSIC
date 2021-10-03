a5f97ea4862c38eb2ac4e30904d3e1f28f175693
SlodeSoft <fab.graglia@gmail.com>
2021-10-03 20:14:57 +0200

    UPDATE PARSE FILE SQL new def

--- PY-MUSIC/MUSIC_THEORY.py
+++ PY-MUSIC/MUSIC_THEORY.py
@@ -24,45 +24,45 @@ class FIND_MODE:
         # Affiche un titre
         print("FIND YOUR MODES - MUSIC THEORY")
         # Initialise Deux listes et l'indice à 0 pour matcher avec la NOTE
-        indice = 0
+        __indice__ = 0
         # Boucle pour chaques MODES
         for x in __MODES__:
             # Affecte l'indice pour la note de départ du mode
             if x == "IONIEN":
-                indice = 9
+                __indice__ = 9
             if x == "DORIAN":
-                indice = 7
+                __indice__ = 7
             if x == "PHRYGIEN":
-                indice = 5
+                __indice__ = 5
             if x == "LYDIEN":
-                indice = 4
+                __indice__ = 4
             if x == "MIXOLYDIEN":
-                indice = 2
+                __indice__ = 2
             if x == "EOLIAN":
-                indice = 0
+                __indice__ = 0
             if x == "LOCRIEN":
-                indice = -2
+                __indice__ = -2
             if x == your_mode or your_mode == "ALL":
                 print("[ " + x + " ]")
                 # Boucle pour chaques notes du mode en cours
                 for y, item in enumerate(__NOTES__, start=0):
                     lmbd = lambda y, b: y + b
-                    print(y, " _ ", __NOTES__[y], " -----> ", __NOTES__[lmbd(y, indice)])
+                    print(y, " _ ", __NOTES__[y], " -----> ", __NOTES__[lmbd(y, __indice__)])
                     # Affecte l'indice pour la note après le dernière note de la liste, soit B = Si
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "IONIEN":
-                        indice = -3
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "DORIAN":
-                        indice = -5
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "PHRYGIEN":
-                        indice = -7
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "LYDIEN":
-                        indice = -8
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "MIXOLYDIEN":
-                        indice = -10
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "EOLIAN":
-                        indice = 0
-                    if __NOTES__[lmbd(y, indice)] == "B" and x == "LOCRIEN":
-                        indice = -2
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "IONIEN":
+                        __indice__ = -3
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "DORIAN":
+                        __indice__ = -5
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "PHRYGIEN":
+                        __indice__ = -7
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "LYDIEN":
+                        __indice__ = -8
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "MIXOLYDIEN":
+                        __indice__ = -10
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "EOLIAN":
+                        __indice__ = 0
+                    if __NOTES__[lmbd(y, __indice__)] == "B" and x == "LOCRIEN":
+                        __indice__ = -2
                 print("")
 
 
